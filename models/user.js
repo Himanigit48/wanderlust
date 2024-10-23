a13ba5b4c95de62a26826551c5f24cc7df6b0232
Himani Rodle <116078235+Himanigit48@users.noreply.github.com>
2024-10-23 16:28:13 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,9 +3,15 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
+ username: {
+    type: String,
+    required: true,
+    unique: true, // Ensure usernames are unique
+  },
   email: {
     type: String,
     required: true,
+    unique: true, // Ensure emails are unique
   },
 });
 
